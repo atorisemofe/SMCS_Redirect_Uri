bd55730374f1e5853e2472d082c6764d0818289c
Oreoluwa Akeredolu <oakeredolu@starmicronics.com>
2022-08-03 12:38:02 -0400

    include header when calling GET /devices

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def about():
 
 @app.route('/devices')
 def devices():
-    #SMCSOauth
+    SMCSOauth
     headers = { "Authorization": "Bearer " + SMCSOauth.access_token }
     list = requests.get("https://device-manager.smcs.io/printer/api/v1/devices", headers=headers)
     data = list.json()
